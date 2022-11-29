8d2a28ed06632c7caaa16f62201c57a429ad41d8
Albert Allagulov <goonnors@gmail.com>
2022-11-29 17:48:45 +0300

    supply websoket protocol

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -9,13 +9,21 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
+import chat.routing
+
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        ),
     }
 )
